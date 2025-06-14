ae08e7ac1e73e47e79c9f985e958b1f6f1a42350
Arex <arekabu@yandex.com>
2025-06-14 14:15:18 +0300

    Models created, migrations made

--- Pereval/settings.py
+++ Pereval/settings.py
@@ -9,8 +9,12 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c1!1fsa8=z8ts5d6$cp79(yw9ezewh^yo$i3fo68e&0a&!%+3l'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'pereval_app',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -74,8 +80,12 @@ WSGI_APPLICATION = 'Pereval.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'pereval',
+        'USER': os.getenv('FSTR_DB_LOGIN'),
+        'PASSWORD': os.getenv('FSTR_DB_PASS'),
+        'HOST': os.getenv('FSTR_DB_HOST'),
+        'PORT': os.getenv('FSTR_DB_PORT'),
     }
 }
 
@@ -115,6 +125,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
---
+++ pereval_app/models.py
@@ -0,0 +1,145 @@
+from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator, FileExtensionValidator
+
+
+class User(models.Model):
+    fam = models.CharField(max_length=50, verbose_name='Фамилия')
+    name = models.CharField(max_length=50, verbose_name='Имя')
+    otc = models.CharField(max_length=50, verbose_name='Отчество')
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=20, blank=True, null=True, verbose_name="Телефон")
+
+    def __str__(self):
+        return f'{self.fam} {self.name} {self.otc or ''}'
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+
+
+class Coords(models.Model):
+    latitude = models.DecimalField(
+        max_digits=7,
+        decimal_places=4,
+        verbose_name='Широта',
+        validators=[MinValueValidator(-90), MaxValueValidator(90)]
+    )
+
+    longitude = models.DecimalField(
+        max_digits=8,
+        decimal_places=4,
+        verbose_name='Долгота',
+        validators=[MinValueValidator(-180), MaxValueValidator(180)]
+    )
+
+    height = models.IntegerField(verbose_name='Высота')
+
+    def __str__(self):
+        return f'{self.latitude}, {self.longitude}, {self.height}.'
+
+    class Meta:
+        verbose_name='Координаты'
+        verbose_name_plural = 'Координаты'
+
+
+class Level(models.Model):
+    LEVEL_CHOICES = [
+        ('1A', '1A'),
+        ('1B', '1B'),
+        ('1C', '1C'),
+        ('2A', '2A'),
+        ('2B', '2B'),
+        ('2C', '2C'),
+        ('3A', '3A'),
+        ('3B', '3B'),
+        ('3C', '3C'),
+    ]
+
+    name = models.CharField(max_length=10, choices=LEVEL_CHOICES, unique=True)
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'Уровень сложности'
+        verbose_name_plural = 'Уровни сложности'
+
+
+class Pereval(models.Model):
+
+    STATUS_CHOICES = [
+        ("new", "Новый"),
+        ("pending", "На проверке"),
+        ("accepted", "Принят"),
+        ("rejected", "Отклонён"),
+    ]
+
+    title = models.CharField(max_length=100)
+    beauty_title = models.CharField(max_length=19, blank=True, null=True)
+    other_titles = models.CharField(max_length=100, blank=True, null=True)
+    connect = models.TextField(max_length=250, blank=True, null=True)
+    add_time = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    coords = models.ForeignKey(Coords, on_delete=models.CASCADE)
+
+    winter = models.ForeignKey(
+        Level,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='winter_perevals',
+        verbose_name="Зима"
+    )
+
+    summer = models.ForeignKey(
+        Level,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='summer_perevals',
+        verbose_name="Лето"
+    )
+
+    autumn = models.ForeignKey(
+        Level,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='autumn_perevals',
+        verbose_name="Осень"
+    )
+
+    spring = models.ForeignKey(
+        Level,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='spring_perevals',
+        verbose_name="Весна"
+    )
+
+    status = models.CharField(max_length=19, choices=STATUS_CHOICES, default='new')
+
+
+class Image(models.Model):
+    pereval = models.ForeignKey(Pereval, on_delete=models.CASCADE, related_name='images', verbose_name="Перевал")
+
+    data = models.ImageField(
+        upload_to='pereval_images/%Y/%m/%d/',
+        verbose_name="Изображение",
+        validators=[
+            FileExtensionValidator(allowed_extensions=['jpg', 'jpeg', 'png'])
+        ]
+    )
+
+    title = models.CharField(max_length=255, verbose_name="Название изображения", blank=True, null=True)
+
+    date_added = models.DateTimeField(auto_now_add=True, verbose_name="Дата добавления")
+
+    class Meta:
+        verbose_name = "Изображение перевала"
+        verbose_name_plural = "Изображения перевалов"
+
+    def __str__(self):
+        return f"{self.title} (Перевал: {self.pereval.title})"
+
