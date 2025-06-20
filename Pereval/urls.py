a029905f3e3f3ec630394e076e52681fa4aed8fd
Arex <arekabu@yandex.com>
2025-06-20 21:26:50 +0300

    urls.py for Swagger edited

--- Pereval/urls.py
+++ Pereval/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('pereval_app.urls')),
+    path('swagger/', TemplateView.as_view(
+        template_name='swagger.html',
+        extra_context={'schema_url': 'openapi-schema'}
+    ), name='swagger-ui'),
 ]
